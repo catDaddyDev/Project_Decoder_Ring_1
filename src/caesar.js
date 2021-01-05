7015d985eff4075bdf27dccda556b0e4461b063f
Elliot Golden <elliotjgolden@gmail.com>
2021-01-04 23:03:34 -0600

    caesar function complete and passing

--- src/caesar.js
+++ src/caesar.js
@@ -1,3 +1,44 @@
-function caesar(input, shift, encode = true) {}
+function caesar(input, shift, encode = true) {
+    if (shift < -25 || shift > 25 || !shift) return false;
+    let finalResult = [];
+    let result = '';
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    input.toLowerCase().split(' ').forEach(word => {
+        word.split('').forEach(character => {
+            if(alphabet.includes(character)) {
+                if (encode) {
+                    const shiftedIndex = alphabet.indexOf(character) + shift;
+                    if (shiftedIndex >= alphabet.length) {
+                        result += alphabet[shiftedIndex - 26];
+                    }
+                    else if (shiftedIndex <= 0) {
+                        result += alphabet[shiftedIndex + 26];
+                    }
+                    else {
+                        result += alphabet[shiftedIndex];
+                    }
+                }
+                else {
+                    const shiftedIndex = alphabet.indexOf(character) - shift;
+                    if (shiftedIndex >= alphabet.length) {
+                        result += alphabet[shiftedIndex - 26];
+                    }
+                    else if (shiftedIndex <= 0) {
+                        result += alphabet[shiftedIndex + 26];
+                    }
+                    else {
+                        result += alphabet[shiftedIndex];
+                    }
+                }
+            } else {
+                result += character;
+            }
+        });
+        finalResult.push(result);
+        result = '';
+    });
+    return finalResult.join(' ');
+}
 
 module.exports = caesar;
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,38 @@
-// Write your tests here!
+const expect = require("chai").expect;
+const caesar = require("../src/caesar");
+
+describe("caesar", () => {
+    it("should return encoded message with forward shift", () => {
+        const actual = caesar("thinkful", 3);
+        const expected = 'wklqnixo';
+        expect(actual).to.eql(expected);
+    });
+    it("should return encoded message with backward shift", () => {
+        const actual = caesar("thinkful", -3);
+        const expected = 'qefkhcri';
+        expect(actual).to.eql(expected);
+    });
+    it("should return decoded message", () => {
+        const actual = caesar("wklqnixo", 3, false);
+        const expected = 'thinkful';
+        expect(actual).to.eql(expected);
+    });
+
+    it("should ignore capitals, maintain spaces and non-alphabetic symbols- encoding", () => {
+        const actual = caesar("This is a secret message!", 8);
+        const expected = 'bpqa qa i amkzmb umaaiom!';
+        expect(actual).to.eql(expected);
+    });
+    it("should ignore capitals, maintain spaces and non-alphabetic symbols- decoding", () => {
+        const actual = caesar("This is a secret message!", 8);
+        const expected = 'bpqa qa i amkzmb umaaiom!';
+        expect(actual).to.eql(expected);
+    });
+
+    it("should return false if the shift value is equal to 0, less than -25, greater than 25, or not present", () => {
+        const actual1 = caesar("thinkful");
+        const actual2 = caesar("thinkful", 99);
+        const actual3 = caesar("thinkful", -26);
+        expect(actual1 && actual2 && actual3).to.be.false;
+    });
+});
