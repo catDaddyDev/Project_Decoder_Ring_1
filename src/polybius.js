6449af51563d56d5e1624a8c03838749e3811539
Elliot Golden <elliotjgolden@gmail.com>
2021-01-06 10:02:17 -0600

    polybius function and tests created

--- src/polybius.js
+++ src/polybius.js
@@ -1,3 +1,50 @@
-function polybius(input, encode = true) {}
+function polybius(input, encode = true) {
+    const alphabet = 
+        {'a':'11', 'b':'21', 'c':'31', 'd':'41', 'e':'51',
+        'f':'12', 'g':'22', 'h':'32', 'i':'42', 'j':'42', 'k': '52',
+        'l':'13', 'm':'23', 'n':'33', 'o':'43', 'p':'53',
+        'q':'14', 'r':'24', 's':'34', 't':'44', 'u':'54',
+        'v':'15', 'w':'25', 'x':'35', 'y':'45', 'z':'55'};
+
+    //Capital letters can be ignored.
+    const lowerCaseInput = input.toLowerCase();
+    let result = '';
+
+    //encoding
+    if (encode) {
+        //loop thru characters
+        for (let i = 0; i < lowerCaseInput.length; i++) {
+            let currentChar = lowerCaseInput[i];
+            if (currentChar === 'i' || currentChar === 'j'){
+                // return 42 for both i/j
+                result += alphabet['i'];
+            } // maintain spaces
+            else if (currentChar === ' ') {
+                result += ' ';
+            } // return corresponding coordinates for letter
+            else {
+                let match = Object.entries(alphabet).find(pair => currentChar === pair[0]);
+                result += match[1];
+            }
+        }
+    } else {
+        //decoding
+        // return false if string of coordinates isn't an even number, excluding spaces
+        if (input.split(' ').join('').length % 2 !== 0) return false;
+        for (i = 0; i < input.length; i+=2) {
+            let coordinate = `${input[i]}${input[i+1]}`;
+            if (coordinate.includes(' ')) {
+                result += ' ';
+                i -= 1;
+            } else if (coordinate == 42) {
+                result += '[i/j]';
+            } else {
+                let match = Object.entries(alphabet).find(pair => coordinate === pair[1]);
+                if (match) result += match[0];
+            }
+        }
+    }
+    return result;
+}
 
 module.exports = polybius;
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,37 @@
+const expect = require("chai").expect;
+const polybius = require("../src/polybius");
 // Write your tests here!
+describe("polybius", () => {
+    it("should return encoded message", () => {
+        const actual = polybius("thinkful");
+        const expected = "4432423352125413";
+        expect(actual).to.equal(expected);
+    });
+    it("should return decoded message", () => {
+        const actual = polybius("3251131343 2543241341", false);
+        const expected = "hello world";
+        expect(actual).to.equal(expected);
+    });
+    it("spaces should be maintained throughout-- capital letters can be ignored", () => {
+        const actual = polybius("Hello world");
+        const expected = "3251131343 2543241341";
+        expect(actual).to.equal(expected);
+    });
+    it("When encoding, your output should still be a string", () => {
+        expect(polybius("Hello world")).to.be.a('string');
+    });
+    it("When decoding, the number of characters in the string excluding spaces should be even. Otherwise, return false.", () => {
+        const actual = polybius("44324233521254134", false);
+        expect(actual).to.be.false;
+    });
+    it("The letters 'i' and 'j' share a space. When encoding, both lettera are converted to 42", () => {
+        const actual = polybius("thinkful");
+        const expected = "4432423352125413";
+        expect(actual).to.equal(expected);
+    });
+    it("The letters 'i' and 'j' share a space. when decoding, both letters should somehow be shown", () => {
+        const actual = polybius("4432423352125413", false);
+        const expected = "th[i/j]nkful";
+        expect(actual).to.equal(expected);
+    });
+});
