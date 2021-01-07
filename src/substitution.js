ebfbda8358cebc3769cc1128f3d3589492c0c46c
Elliot Golden <elliotjgolden@gmail.com>
2021-01-07 10:44:02 -0600

    substitution function and tests complete

--- src/substitution.js
+++ src/substitution.js
@@ -1,3 +1,44 @@
-function substitution(input, alphabet, encode = true) {}
+function substitution(input, alphabet, encode = true) {
+    // array of alphabet characters
+    const trueAlphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    const inputLowerCase = input.toLowerCase();  // ignores capitals
+
+    let result  = ''; // will hold encoded/decoded string
+
+    // should return false if 'alphabet' parameter isn't exactly 26
+    if (alphabet.length !== 26) return false;
+
+    // should return false if all of the characters in the 'alphabet' aren't unique (duplicates)
+    if (Array.from(new Set(alphabet)).length !== 26) return false;
+
+    // should return false if alphabet does not contain all characters in the input, removes spaces and letters back
+
+    for (let char of inputLowerCase.split(' ').join('')) {
+        if (!alphabet.includes(char)) {
+            return false;
+        }
+    }
+
+    //loop through the input string
+    for (let i = 0; i < inputLowerCase.length; i++) {
+
+        //spaces maintained throughout
+        if (inputLowerCase[i] === ' ') {
+            result += ' ';
+        } else {
+            if (encode) {
+                let newIndex = trueAlphabet.indexOf(inputLowerCase[i]);
+                result += alphabet[newIndex];
+            } else {
+                let newIndex = alphabet.indexOf(inputLowerCase[i]);
+                result += trueAlphabet[newIndex];
+            }
+        }
+
+    }
+
+  return result;
+}
 
 module.exports = substitution;
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,32 @@
+const expect = require('chai').expect;
+const substitution = require('../src/substitution');
 // Write your tests here!
+describe('substitution', () => {
+    it("should return encoded message", () => {
+        const actual = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev");
+        const expected = 'jrufscpw';
+        expect(actual).to.eql(expected);
+    });
+    it("should return decoded message", () => {
+        const actual = substitution("jrufscpw", "xoyqmcgrukswaflnthdjpzibev", false);
+        const expected = 'thinkful';
+        expect(actual).to.eql(expected);
+    });
+    it("should return encoded message with spaces maintained throughout and capitals ignored", () => {
+        const actual = substitution("You are an excellent spy", "xoyqmcgrukswaflnthdjpzibev");
+        const expected = 'elp xhm xf mbymwwmfj dne';
+        expect(actual).to.eql(expected);
+    });
+    it("should return false if the 'alphabet' parameter is not exactly a string of 26 characters", () => {
+        const actual = substitution("thinkful", "short");
+        expect(actual).to.be.false;
+    });
+    it("should return false if all of the characters in the 'alphabet' parameter are not unique", () => {
+        const actual = substitution("thinkful", "abcabcabcabcabcabcabcabcyz");
+        expect(actual).to.be.false;
+    });
+    it("Alphabet should contain all characters in the input, return false otherwise", () => {
+        const actual = substitution('jrufscpw', 'xoyqmcgrukswaflnthd!pzibev', false);
+        expect(actual).to.be.false;
+    });
+});
